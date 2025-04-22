b3b3b1e618c7f0ee3a81d441722047d6a7d2290b
Emerson <emersontec147@gmil.com>
2025-04-22 12:25:36 -0500

    Se modifico el auth controllador con nuevos endpoints y los demas de config

--- src/main/java/com/emersondev/service/interfaces/AuthService.java
+++ src/main/java/com/emersondev/service/interfaces/AuthService.java
@@ -1,15 +1,22 @@
 package com.emersondev.service.interfaces;
 
+import com.emersondev.api.request.CambiarPasswordRequest;
 import com.emersondev.api.request.LoginRequest;
 import com.emersondev.api.request.RegistroRequest;
 import com.emersondev.api.response.JwtResponse;
 
 public interface AuthService {
+
   JwtResponse login(LoginRequest loginRequest);
 
   JwtResponse registro(RegistroRequest registroRequest);
 
   JwtResponse refreshToken(String refreshToken);
 
-  void logout();
+  void cambiarPassword(CambiarPasswordRequest cambiarPasswordRequest);
+
+  void logout(String token);
+
+  boolean validarToken(String token);
+
 }
